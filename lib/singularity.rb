371b970ef8f18e50df4b6b8f94bad1e314123572
Travis Webb <d.webb@utexas.edu>
2016-09-14 15:43:40 -0500

    runner function

--- lib/singularity.rb
+++ lib/singularity.rb
@@ -87,8 +87,27 @@ module Singularity
       @data = JSON.parse(@config.result(@r.get_binding))
       print @data['id']
     end
-
     def runner
-
+      begin
+        if is_paused()
+          puts " PAUSED, SKIPPING".yellow
+          return
+        else
+          # create or update the request
+          # resp = RestClient.post "#{@uri}/api/requests", @data.to_json, :content_type => :json
+        end
+        # deploy the request
+        # @data['requestId'] = @data['id']
+        # @data['id'] = "#{@release}.#{Time.now.to_i}"
+        # deploy = {
+        #  'deploy' => @data,
+        #  'user' => `whoami`.chomp,
+        #  'unpauseOnSuccessfulDeploy' => false
+        # }
+        # resp = RestClient.post "#{@uri}/api/deploys", deploy.to_json, :content_type => :json
+        # puts " DEPLOYED".green
+      rescue Exception => e
+        puts " #{e.response}".red
+      end
     end
 end
