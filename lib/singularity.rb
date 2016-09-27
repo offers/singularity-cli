1702c58a93a3ee6860beb88a9d07f8040bac4f40
Travis Webb <d.webb@utexas.edu>
2016-09-27 12:29:08 -0500

    updates

--- lib/singularity.rb
+++ lib/singularity.rb
@@ -184,6 +184,8 @@ module Singularity
         puts ""
         # the below line is me trying to figure out how to output the STDOUT/STDERR to the shell, not working yet
         # puts "DELETED REQUEST: ".orange
+        request = RestClient.get "#{@uri}/api/requests/request/#{@data['requestId']}"
+        puts request.to_json
         # puts RestClient.delete "#{@uri}/api/requests/request/#{@data['requestId']}" 
         ########################################################
         # NEED TO DELETE THE REQUEST AFTER ALL OF THIS IS OVER #
