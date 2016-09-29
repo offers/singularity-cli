8a8cf2803cd952de7236dd78fbae23c035eaa62a
Travis Webb <d.webb@utexas.edu>
2016-09-29 12:16:07 -0500

    updates

--- lib/singularity.rb
+++ lib/singularity.rb
@@ -206,8 +206,7 @@ module Singularity
           @port = @thisTask['mesosTask']['container']['docker']['portMappings'][0]['hostPort']
           # SSH into the machine
           sleep 3
-          authfile = Dir.pwd + "/docker/api/ssh-key.pub"
-          exec "ssh -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@#{@ip} -p #{@port} -v -i #{authfile}"
+          exec "ssh -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@#{@ip} -p #{@port} -v"
         else
           # or provide link to task in browser so we can see output
           puts " Deployed and running #{@data['command']} #{@data['arguments']}".green
