75b17b3149adc2b642693409d04a0f933946868e
Travis Webb <d.webb@utexas.edu>
2016-10-25 14:04:12 -0500

    updates

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -9,19 +9,27 @@ module Singularity
     context 'when paused' do
       WebMock.stub_request(:get, @test_url).
         to_return(body: hash_including({state: 'PAUSED'}))
-      it "should find paused == true" do
-        response = @request.is_paused
-        expect(WebMock).to have_requested(:get, @test_url+'/api/requests/request/'+@test_id).and 
-        expect(response).to equal(true)
+
+      @response = @request.is_paused
+
+      it "should make a GET request"
+        expect(WebMock).to have_requested(:get, @test_url+'/api/requests/request/'+@test_id)
       end
+      it "should find PAUSED == true" do
+        expect(response).to equal(true)
+      end 
     end
 
     context 'when not paused' do
       WebMock.stub_request(:get, @test_url).
         to_return(body: hash_including({state: 'RUNNING'}))
-      it "should find paused == false" do
-        response = @request.is_paused
-        expect(WebMock).to have_requested(:get, @test_url+'/api/requests/request/'+@test_id).and
+
+      @response = @request.is_paused
+      
+      it "should make a GET request"
+        expect(WebMock).to have_requested(:get, @test_url+'/api/requests/request/'+@test_id)
+      end
+      it "should find PAUSED == false" do
         expect(response).to equal(false)
       end
     end
