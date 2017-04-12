36b72af847ff753207b9ae03c2eeab000b5d7877
Travis Webb <travis.w.dev@gmail.com>
2017-04-12 16:34:53 -0500

    list ssh working, needs more pretty

--- lib/singularity/request.rb
+++ lib/singularity/request.rb
@@ -27,15 +27,57 @@ module Singularity
     def list_ssh
       tasks = JSON.parse(RestClient.get "#{@uri}/api/tasks/active", :content_type => :json)
 
+      count = 1
       tasks.each do |entry|
         taskId = entry['taskRequest']['request']['id']
         if taskId.include?("SSH")
           ip = entry['offer']['url']['address']['ip']
           port = entry['mesosTask']['container']['docker']['portMappings'][0]['hostPort']
 
-          puts "#{taskId}: ".light_blue + "root".yellow + " @ ".light_blue + "#{ip}".light_magenta + " : ".light_blue + "#{port}".light_cyan
+          puts "#{count}: ".light_green + "#{taskId}: ".light_blue + "root".yellow + " @ ".light_blue + "#{ip}".light_magenta + " : ".light_blue + "#{port}".light_cyan
+          count = count + 1
         end
       end
+
+      puts "Would you like to (k)ill or (c)onnect to any of these sessions? (n or Ctrl+C to exit) "
+      resp = 'x'
+
+      while !['n','k','kill','c','con','conn','connect'].include?(resp)
+        resp = gets
+      end
+
+      case resp
+        when 'n'
+          message = RestClient.get "http://bit.ly/2p9K3sI"
+          message = message.chop.chop.chop + "\""
+          puts RestClient.get "http://bit.ly/2otyRsW", :message => "#{message}", :format => "text"
+        when 'k','kill'
+
+        when 'c','con','conn','connect'
+          # please enter the session number
+
+      #   # wrap the below in error checking
+      #   num = gets.to_i
+
+
+      #   while resp != (y|n)
+      #     puts "SSH into <> (y/n)?"
+      #     resp = getc
+      #   end
+      #  if resp == "y"
+      #   Singularity::Runner ->ssh
+      #  else
+      # end
+
+      # if (k)
+      #   please enter a comma-separated list of which numbers you'd like to kill
+      #   list = gets
+      #   list = list.remove(" ").split(",")
+      #   list.each do |process_index|
+      #     tasks[process_index-1]
+      #     "!!".red + " Are you sure you want to KILL " + "#{taskId}}".red + "?" + "!!".red
+      #   end
+      # end
 
     end
 
